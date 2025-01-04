1d517c3bee67b12b0948794c53c15782080b48a7
my99wins <my999n@gmail.com>
2025-01-04 13:09:19 +0700

    test from wins

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,20 +7,21 @@ import Image from "next/image"
 
 export default function Page() {
   return (
-    <Box textAlign="center" fontSize="xl" pt="30vh">
-      <VStack gap="8">
-        <Image
-          alt="chakra logo"
-          src="/static/logo.svg"
-          width="80"
-          height="80"
-        />
-        <Heading size="2xl" letterSpacing="tight">
-          Welcome to the template site - Powered by Chakra
-        </Heading>
+    <>
+      <Box textAlign="center" fontSize="xl" pt="30vh">
+        <VStack gap="8">
+          <Image
+            alt="chakra logo"
+            src="/static/logo.svg"
+            width="80"
+            height="80"
+          />
+          <Heading size="2xl" letterSpacing="tight">
+            Welcome to the template site - Powered by Chakra
+          </Heading>
 
-      </VStack>
-
-    </Box>
+        </VStack>
+      </Box>
+    </>
   )
 }
