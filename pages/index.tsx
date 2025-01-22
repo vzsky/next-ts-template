d3e04bc035422d975eb775c51e8a14acabb8d896
vzsky <my999n@gmail.com>
2025-01-22 23:46:17 +0700

    add clock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,60 @@
 import {
+  AbsoluteCenter,
   Box,
   Heading,
-  VStack,
 } from "@chakra-ui/react"
-import Image from "next/image"
+import { useState } from 'react'
 
-export default function Page() {
+const useClock = () => {
+  const [hour, setHour] = useState<number>()
+  const [minute, setMinute] = useState<number>()
+  const [second, setSecond] = useState<number>()
+  const [day, setDay] = useState<number>()
+  const [month, setMonth] = useState<number>()
+  const [year, setYear] = useState<number>()
+
+  setTimeout(() => {
+    const date = new Date()
+    setHour(date.getHours())
+    setMinute(date.getMinutes())
+    setSecond(date.getSeconds())
+    setDay(date.getDay())
+    setMonth(date.getMonth() + 1)
+    setYear(date.getFullYear())
+  }, 1000); 
+
+  return {hour, minute, second, day, month, year}
+}
+
+const useSimpleClock = () => {
+  const { hour, minute, second, day, month, year } = useClock()
+  if (!hour || !minute || !second || !day || !month || !year) return ["loading", "..."]
+  let h = hour.toString(); if (hour < 10) h = "0" + h;
+  let m = minute.toString(); if (minute < 10) m = "0" + m;
+  let s = second.toString(); if (second < 10) s = "0" + s;
+  return [`${h}:${m}:${s}`, `${day} / ${month} / ${year}`]
+}
+
+const ClockDisplay = ({clock}: any) => {
+  const [line1, line2] = clock();
   return (
     <>
-      <Box textAlign="center" fontSize="xl" pt="30vh">
-        <VStack gap="8">
-          <Image
-            alt="chakra logo"
-            src="/static/logo.svg"
-            width="80"
-            height="80"
-          />
-          <Heading size="2xl" letterSpacing="tight">
-            Welcome to the template site - Powered by Chakra
-          </Heading>
-
-        </VStack>
-      </Box>
+      <Heading fontFamily="Monaco" size="7xl" letterSpacing="tight">
+        {line1}
+      </Heading>
+      <Heading fontFamily="Monaco" size="5xl" letterSpacing="tight">
+        {line2}
+      </Heading>
     </>
   )
 }
+
+export default () => (
+  <>
+    <AbsoluteCenter>
+      <Box textAlign="center">
+        <ClockDisplay clock={useSimpleClock}/>
+      </Box>
+    </AbsoluteCenter>
+  </>
+)
